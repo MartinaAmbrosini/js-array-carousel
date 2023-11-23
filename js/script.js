6d3e745b649fef659d44acd0ce7b807f9a095284
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-11-23 16:29:48 +0100

    ⚡️

--- js/script.js
+++ js/script.js
@@ -6,3 +6,44 @@
 
 // MILESTONE 2
 // Al click dell’utente sulle frecce (sia Prev che Next), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+const items = document.getElementsByClassName("item");
+console.log(items)
+
+let activeItem = 0;
+
+// navigazione
+const next = document.querySelector(".next");
+
+next.addEventListener("click", function() {
+
+    if (activeItem < items.length - 1) {
+        // togliamo la classe active all'elemento corrente 
+        items[activeItem].classList.remove("active");
+
+        // incremento indice dell'elemento per andare all'elemento successivo
+        activeElement++;
+
+        // aggiungo classe active al prossimo elemento
+        items[activeItem].classList.add("active");
+    }
+
+});
+
+// seleziono back
+const back = document.querySelector(".back");
+
+// azioni al click del bottone back
+back.addEventListener("click", function(){
+
+    if(activeElement > 0){
+        // tolgo classe active all'elemento precedente
+        element[activeElement].classList.remove("active");
+
+        // i meno incidono sull'elemento per andare all'elemento precedente 
+        activeElement--;
+
+        // aggiungo classe active all'elemento
+        element[activeElement].classList.add("active");
+    }
+});
